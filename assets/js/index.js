360884baa65d00147450a2f30c09c64a3556726a
zekeriyauysal <162806897+zekeriyauysal@users.noreply.github.com>
2024-03-09 17:12:36 +0300

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,104 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const quizContainer = document.getElementById("quiz-container");
+  let currentQuestionIndex = 0;
+  let questions = [];
+  let timerId;
+
+  function getQuizQuestions() {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((data) => {
+        questions = data.slice(0, 10).map((question) => ({
+          title: question.title,
+          options: question.body.split("\n").map((option) => option.trim()),
+        }));
+        displayQuestion(questions[currentQuestionIndex]);
+      })
+      .catch((error) => console.error("Error:", error));
+  }
+
+  function displayQuestion(question) {
+    clearTimeout(timerId);
+    quizContainer.innerHTML = `
+            <div class="card">
+                <div class="card-header d-flex justify-content-between">
+                 <span>Soru ${currentQuestionIndex + 1}</span>
+                 <span id="timer"></span>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">${question.title}</h5>
+                    <div id="options-container" class="mt-4">
+                        ${question.options
+                          .map(
+                            (option, index) => `
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="questionOption" id="option${index}" data-index="${index}" disabled>
+                                <label class="form-check-label" for="option${index}">
+                                    ${String.fromCharCode(65 + index)}) ${option}
+                                </label>
+                            </div>
+                        `
+                          )
+                          .join("")}
+                    </div>
+                    <button id="next-btn" class="btn btn-primary mt-3" disabled>İleri</button>
+                </div>
+            </div>
+        `;
+
+    document.getElementById("next-btn").addEventListener("click", goToNextQuestion);
+    startReadingPeriod();
+  }
+
+  function startReadingPeriod() {
+    let readTime = 10;
+    updateTimer(readTime);
+
+    timerId = setInterval(() => {
+      readTime--;
+      updateTimer(readTime);
+      if (readTime <= 0) {
+        clearInterval(timerId);
+        document.getElementById("next-btn").disabled = false;
+        startAnsweringPeriod();
+      }
+    }, 1000);
+  }
+
+  function startAnsweringPeriod() {
+    document.querySelectorAll("#options-container .form-check-input").forEach((input) => {
+      input.disabled = false;
+    });
+
+    let answerTime = 30;
+    updateTimer(answerTime);
+
+    timerId = setInterval(() => {
+      answerTime--;
+      updateTimer(answerTime);
+      if (answerTime <= 0) {
+        clearInterval(timerId);
+        goToNextQuestion();
+      }
+    }, 1000);
+  }
+
+  function updateTimer(time) {
+    const timerElement = document.getElementById("timer");
+    timerElement.textContent = `Kalan Süre: ${time}s`;
+  }
+
+  function goToNextQuestion() {
+    clearInterval(timerId);
+    currentQuestionIndex++;
+    if (currentQuestionIndex < questions.length) {
+      displayQuestion(questions[currentQuestionIndex]);
+    } else {
+      quizContainer.innerHTML = `<div class="alert alert-success" role="alert">
+                Quiz cevapları
+            </div>`;
+      clearTimeout(timerId);
+    }
+  }
+  getQuizQuestions();
+});
